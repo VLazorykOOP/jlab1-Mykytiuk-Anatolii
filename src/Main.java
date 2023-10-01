5e3e72865f65af0eb0bc6a0b60785349d45cb7e8
Mykytiuk-Anatolii <mykytiuk.anatolii@chnu.edu.ua>
2023-10-01 17:18:36 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,65 @@ public class Main {
 
 
 public static void First() {
-
+    Scanner s = new Scanner(System.in);
+    System.out.println("Select variant of data:");
+    System.out.println("Input valid type, result - valid");
+    System.out.println("Input integer type, result - valid ");
+    System.out.println("Input valid type, result - integer");
+    int choice = s.nextInt();
+    switch (choice){
+        case 1: {
+            float x, y;
+            System.out.println("Input x");
+            x = s.nextFloat();
+            System.out.println("Input y");
+            y = s.nextFloat();
+            // Перевірка //
+            if (x == 0 || y == 0) {
+                throw new IllegalArgumentException("Error: The denominator is equal 0.");
+            } else {
+                float res = ((2 * x * x + x * y) / (x * y) * (x * y))
+                            + (((3 * x * y) - (y * y * y)) / (x * x) + (2 * y * y));
+                    System.out.println("Option 1: " + res);
+                    break;
+            }
+        }
+        case 2: {
+            int x, y;
+            System.out.println("Enter x");
+            x = s.nextInt();
+            System.out.println("Enter y");
+            y = s.nextInt();
+            if (x == 0 || y == 0) {
+                throw new IllegalArgumentException("Error: The denominator is equal 0.");
+            } else {
+                float res = ((2 * x * x + x * y) / (x * y) * (x * y))
+                        + (((3 * x * y) - (y * y * y)) / (x * x) + (2 * y * y));
+                System.out.println("Option 2: " + res);
+                break;
+            }
+        }
+        case 3: {
+            float x, y;
+            System.out.println("Enter x");
+            x = s.nextFloat();
+            System.out.println("Enter y");
+            y = s.nextFloat();
+            if (x == 0 || y == 0) {
+                throw new IllegalArgumentException("Error: The denominator is equal 0.");
+            } else {
+                float res = ((2 * x * x + x * y) / (x * y) * (x * y))
+                        + (((3 * x * y) - (y * y * y)) / (x * x) + (2 * y * y));
+                int result = (int) res;
+                System.out.println("Option 3: " + result);
+                break;
+            }
+        }
+        default: {
+            System.out.print("Please input normal number");
+            break;
+        }
+    }
 }
 public static void Second() {
 
