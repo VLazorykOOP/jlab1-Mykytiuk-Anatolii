99698553054c312c4e1f9650c186b23e9a2c7228
Mykytiuk-Anatolii <mykytiuk.anatolii@chnu.edu.ua>
2023-10-01 16:55:58 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Lab 1 Java");
+	Scanner s = new Scanner(System.in);
+    System.out.println("Lab 1 Java");
+    System.out.println("Select the task by 1 for 4:");
+    int choice = s.nextInt();
+    switch (choice) {
+        case 1: {
+            First();
+            break;
+        }
+        case 2: {
+            Second();
+            break;
+        }
+        case 3: {
+            Third();
+            break;
+        }
+        case 4: {
+            Fourth();
+            break;
+        }
+        default: {
+            System.out.print("Pleas enter normal number:");
+            break;
+        }
     }
+    }
+
+
+public static void First() {
+
+}
+public static void Second() {
+
+}
+public static void Third(){
+
+}
+public static void Fourth() {
+
+}
+
 }
